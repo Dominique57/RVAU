02d126ec58edc27ad2104dbee4dcdb3f96900ddb
Dominique57 <Dominique57@users.noreply.github.com>
2022-02-01 19:12:16 +0100

    Add lever / gamemanager script for lever code to do smth

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -15,6 +16,21 @@ public class GameManager : MonoBehaviour {
     public ColliderListener greenBadgeCollider;
     public DoorOpeningWithLock greenBadgeStepDoor;
 
+    public LeverActivation leverDoor1;
+    public LeverActivation leverDoor2;
+    public LeverActivation leverDoor3;
+    public bool leverCodeCorrect = false;
+
+    protected struct LeverCode {
+        public LeverActivation _lever;
+        public bool _enabled; // true => Enabled // false => Disabled
+        public LeverCode(LeverActivation lever, bool enabled) {
+            _lever = lever;
+            _enabled = enabled;
+        }
+    }
+    protected List<LeverCode> _leverCodes = new List<LeverCode>();
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +42,20 @@ public class GameManager : MonoBehaviour {
         greenBadgeStepDoor.locked = !greenDoorUnlocked;
         
         greenBadgeCollider.TriggerEnterListener.AddListener(OnGreenTriggerEnter);
+        leverDoor1.EnableEvent.AddListener(OnLeverDoorToggle);
+        leverDoor1.DisableEvent.AddListener(OnLeverDoorToggle);
+        leverDoor2.EnableEvent.AddListener(OnLeverDoorToggle);
+        leverDoor2.DisableEvent.AddListener(OnLeverDoorToggle);
+        leverDoor3.EnableEvent.AddListener(OnLeverDoorToggle);
+        leverDoor3.DisableEvent.AddListener(OnLeverDoorToggle);
+
+        _leverCodes.Clear();
+        _leverCodes.Add(new LeverCode(leverDoor1, true));
+        _leverCodes.Add(new LeverCode(leverDoor2, true));
+        _leverCodes.Add(new LeverCode(leverDoor3, false));
+        
+        if (leverCodeCorrect)
+            OnLeverCodeCorrect();
     }
 
     // Called only in editor mode when a variable is changed
@@ -64,5 +94,42 @@ public class GameManager : MonoBehaviour {
     public void OnGreenTriggerEnter(Collider other) {
         if (other.CompareTag("Badge"))
             greenBadgeStepDoor.locked = false;
+    }
+
+    
+    public String LevelCodeToString() {
+        var builder = new StringBuilder();
+        foreach (var leverCode in _leverCodes) {
+            if (leverCode._enabled) {
+                builder.Append(leverCode._lever.Enabled? "OK": "KO");
+            } else {
+                builder.Append(leverCode._lever.Disabled? "OK": "KO");
+            }
+            builder.Append(" ");
+        }
+        return builder.ToString();
+    }
+    
+    public bool CheckLevelCode() {
+        foreach (var leverCode in _leverCodes) {
+            if (leverCode._enabled) {
+                if (!leverCode._lever.Enabled)
+                    return false;
+            } else {
+                if (!leverCode._lever.Disabled)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    protected void OnLeverDoorToggle(Collider other) {
+        leverCodeCorrect = CheckLevelCode();
+        if (leverCodeCorrect)
+            OnLeverCodeCorrect();
+    }
+
+    public void OnLeverCodeCorrect() {
+        
     }
 }
